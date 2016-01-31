7e09ae5bb736ce252970dfb7459c0cb2cbf7a688
Tom Kidd <tomkidd@gmail.com>
2016-01-31 11:07:20 -0600

    Slimmed down some

--- index.php
+++ index.php
@@ -1,24 +1,21 @@
 <?php
-
 date_default_timezone_set("America/Chicago");
-
 $xml=("http://www.idlethumbs.net/feeds/idle-thumbs");
-
 $xmlDoc = new DOMDocument();
 $xmlDoc->load($xml);
 
-$item = $xmlDoc->getElementsByTagName('item')->item(0);
-$title = $item->getElementsByTagName('title')->item(0)->nodeValue;
+// get latest episode title
+$title = $xmlDoc->getElementsByTagName('item')->item(0)->getElementsByTagName('title')->item(0)->nodeValue;
 
-$pieces = explode(':', $title);
-$current_episode = explode(" ", $pieces[0]);
+// extrapolate number, add one to it
+$current_episode = intval(explode(" ", explode(':', $title)[0])[2]) + 1;
 
 echo("<h1>It's ");
 echo(date("F jS, Y"));
 echo("</h1>");
 
 echo("<h1>This is Idle Thumbs ");
-echo(intval($current_episode[2]) + 1);
+echo($current_episode);
 echo("</h1>");
 
 ?>
